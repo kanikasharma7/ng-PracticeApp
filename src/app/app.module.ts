96ee42fc36e41b252c379091f787c6940e6b7b4b
kanikasharma7 <31842158+kanikasharma7@users.noreply.github.com>
2018-11-07 01:11:42 +0530

    Assignment changes done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule
